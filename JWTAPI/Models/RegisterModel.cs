422298614af849b64ebf0a96a32c8a78f54e1db9
Binh <tongbinh159@gmail.com>
2023-05-05 21:05:03 +0700

    Add project files.

---
+++ JWTAPI/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWTAPI.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Full Name is required")]
+        public string? Fullname { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
---
+++ JWTAPI/Models/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿namespace JWTAPI.Models
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string VPCD = "User";
+        public const string BM = "User";
+        public const string SaleUp = "User";
+
+
+    }
+}
