ada631346337df56d77f3924607a313681f9b7ae
Binh <tongbinh159@gmail.com>
2023-05-17 14:17:05 +0700

    17/5/2023

---
+++ JWTAPI/Models/ModelArea.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace JWTAPI.Models
+{
+    public class ModelArea
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Key] public int IdArea { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? NameArea { get; set; }
+        public string? Distributor { get; set; }
+    }
+}
